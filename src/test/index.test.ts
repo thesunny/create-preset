76f1671216e223fe2d4ed0345a0e51b122ca22ac
Sunny Hirai <thesunny@gmail.com>
2022-02-08 22:51:12 -0800

    Add comment to empty unit tests

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,5 +1,15 @@
 export {}
 
 describe("index", () => {
-  it("should pass", async () => {})
+  it("should pass", async () => {
+    /**
+     * We don't have any unit tests because the code executes a few commands
+     * and then ends.
+     *
+     * It's not really possible to test except by using it.
+     *
+     * Never the less, we include a unit test here so that we can keep the
+     * repo consistent with the other npm repos which use the presets.
+     */
+  })
 })
